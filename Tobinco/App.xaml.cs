531ff0311ac1a26fff00b17d1e9433890aff32e7
Emmanuel Morrison <neo2morris@live.com>
2024-01-12 00:37:29 +0000

    first commit

---
+++ Tobinco/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Tobinco.ViewPages;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tobinco
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new TobincoSplash();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Tobinco/ViewPages/ConfrimMedicalRequest.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Tobinco.ViewPages
+{
+    public partial class ConfrimMedicalRequest 
+    {
+        public ConfrimMedicalRequest(int DoctorId, string DocImageUrl, long DocTel, string DocTitle, string FirstName, string LastName)
+        {
+            InitializeComponent();
+        }
+
+        async void ClosePostBill(object sender, EventArgs e)
+        {
+        }
+    }
+}
